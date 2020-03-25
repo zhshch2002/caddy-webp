3faec0f66a9225d693436e721c89cc58ad54c224
zhshch2002 <zhshch@athorx.com>
2020-03-25 08:55:57 +0800

    update

--- caddywebp.go
+++ caddywebp.go
@@ -2,15 +2,16 @@ package github
 
 import (
 	"bytes"
-	"github.com/caddyserver/caddy"
-	"github.com/caddyserver/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"github.com/chai2010/webp"
-	log "github.com/sirupsen/logrus"
 	"golang.org/x/image/bmp"
 	"image"
 	"image/jpeg"
 	"image/png"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -18,39 +19,37 @@ import (
 const Quality = 80
 
 func init() {
-	log.Println("RegisterPlugin")
-	caddy.RegisterPlugin("webp", caddy.Plugin{
-		ServerType: "http",
-		Action:     setup,
-	})
+	log.Println("webp plugin")
+	err := caddy.RegisterModule(Webp{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpcaddyfile.RegisterHandlerDirective("webp", parseCaddyfile)
 }
 
-func setup(c *caddy.Controller) error {
-	log.Println("setupFunc")
-	h := handler{}
-	//for c.Next() {
-	//
-	//}
-	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
-		h.next = next
-		return h
-	})
-	return nil
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	return Webp{}, nil
 }
 
-type handler struct {
-	next httpserver.Handler
+type Webp struct {
 }
 
-func (s handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+func (Webp) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		Name: "http.handlers.webp",
+		New:  func() caddy.Module { return new(Webp) },
+	}
+}
+
+func (s Webp) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	ua := r.Header.Get("User-Agent")
 	if strings.Contains(ua, "Safari") && !strings.Contains(ua, "Chrome") && !strings.Contains(ua, "Firefox") {
-		return s.next.ServeHTTP(w, r) // 对Safari禁用webp
+		return next.ServeHTTP(w, r) // 对Safari禁用webp
 	}
 	resp := &response{}
-	i, err := s.next.ServeHTTP(resp, r)
+	err := next.ServeHTTP(resp, r)
 	if err != nil {
-		return i, err
+		return err
 	}
 	ct := http.DetectContentType(resp.Body.Bytes())
 
@@ -59,35 +58,35 @@ func (s handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) 
 	var decoder func(io.Reader) (image.Image, error)
 	if strings.Contains(ct, "jpeg") {
 		decoder = jpeg.Decode
-	} else if strings.Contains(ct, ".png") {
+	} else if strings.Contains(ct, "png") {
 		decoder = png.Decode
-	} else if strings.Contains(ct, ".bmp") {
+	} else if strings.Contains(ct, "bmp") {
 		decoder = bmp.Decode
 		// } else if strings.HasSuffix(r.URL.String(), ".gif") { TODO need to support animated webp
 		// 	decoder = gif.Decode
 	} else {
-		return s.next.ServeHTTP(w, r)
+		return next.ServeHTTP(w, r)
 	}
 
 	img, err := decoder(bytes.NewReader(resp.Body.Bytes()))
 	if err != nil || img == nil {
-		log.Error(err)
-		return s.next.ServeHTTP(w, r)
+		log.Println(err)
+		return next.ServeHTTP(w, r)
 	}
 	var buf bytes.Buffer
 	err = webp.Encode(&buf, img, &webp.Options{Lossless: false, Quality: Quality})
 	if err != nil {
-		log.Error(err)
-		return s.next.ServeHTTP(w, r)
+		log.Println(err)
+		return next.ServeHTTP(w, r)
 	}
 	w.Header().Set("Content-Type", "image/webp")
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write(buf.Bytes())
 	if err != nil {
-		log.Error(err)
-		return http.StatusInternalServerError, err
+		log.Println(err)
+		return err
 	}
-	return http.StatusOK, nil
+	return nil
 }
 
 type response struct {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/zhshch2002/caddy-webp
 go 1.14
 
 require (
-	github.com/caddyserver/caddy v1.0.5
+	github.com/caddyserver/caddy/v2 v2.0.0-beta9 // indirect
 	github.com/chai2010/webp v1.1.0
 	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/image v0.0.0-20200119044424-58c23975cae1
